074b21ad61cc24c2206a71134a2de065f7615b3f
hanlaf <hanlaf07@gmail.com>
2023-11-16 09:51:08 +0000

    Start work on creating teams

--- tasks/models.py
+++ tasks/models.py
@@ -17,7 +17,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
     email = models.EmailField(unique=True, blank=False)
-
+    #teams = models.ManyToManyField(Team)
 
     class Meta:
         """Model options."""
@@ -40,3 +40,46 @@ class User(AbstractUser):
         """Return a URL to a miniature version of the user's gravatar."""
         
         return self.gravatar(size=60)
+
+class Team(models.Model):
+    """Model used to hold teams of different users and their relevant information"""
+    
+    team_name = models.CharField(max_length=50, unique=True, blank=False)
+    users = models.ManyToManyField(User)
+    description = models.TextField(max_length=200, blank=True)
+
+    def add_member(self, newUser):
+        """Add a new user to the users field"""
+        # May have to add privileges as well?
+        self.users.add(newUser)
+        self.save()
+    
+    def get_all_members(self):
+        """Returns query set containing all the users in team"""
+        return self.users.all()
+    
+    def remove_member(self, user_to_remove):
+        """Removes user from team"""
+        # May have to remove privileges as well?
+        self.users.remove(user_to_remove)
+        self.save()
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+
+    class Meta:
+        ordering = ["headline"]
+
+    def __str__(self):
+        return self.headline
