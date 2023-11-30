e81078c5209b7a09a3e597e8995f7c48e3087f65
Anna1021 <k22008684@kcl.ac.uk>
2023-11-30 11:32:12 +0000

    bug fix

--- tasks/tests/views/test_task_search_view.py
+++ tasks/tests/views/test_task_search_view.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from tasks.models import Task
 from tasks.tests.helpers import LogInTester
+from datetime import datetime
+
 from django.http import HttpResponseBadRequest
 
 class TaskSearchViewTests(TestCase, LogInTester):
@@ -14,10 +16,29 @@ class TaskSearchViewTests(TestCase, LogInTester):
         response = self.client.get(reverse('task_search'), {'q': 'Task 1'})
         #print(response.content)
         self.assertEqual(response.status_code, 200)
+        for x in response.context:
+            print(x)
+
+        print(response.context['data'][0].due_date)
+        print("shdjshdjshjdsh")
         self.assertContains(response, 'Task 1')
         self.assertContains(response, 'Description 1')
-       # self.assertContains(response, "2023-12-01 00:00:00")
+        self.assertContains(response, '2023-12-01 00:00:00+00:00')
 
+    def setUp(self):
+        Task.objects.create(name="Task 1", description="Description 1", due_date="2023-12-01")
+        Task.objects.create(name="Task 2", description="Description 2", due_date="2023-12-02")
+        Task.objects.create(name="Task 3", description="Description 3", due_date="2023-12-03")
+
+    def test_search_view_with_results(self):
+        response = self.client.get(reverse('task_search'), {'q': 'Task 1'})
+        self.assertEqual(response.status_code, 200)
+        #print(response.context['data'][0].due_date)
+        print("shdjshdjshjdsh")
+        self.assertContains(response, 'Task 1')
+        self.assertContains(response, 'Description 1')
+        print(response.context)
+        self.assertEquals(response.context['data'][0].due_date.__str__(), '2023-12-01 00:00:00+00:00')
 
     def test_search_view_no_results(self):
         response = self.client.get(reverse('task_search'), {'q': 'Non-existent task'})
