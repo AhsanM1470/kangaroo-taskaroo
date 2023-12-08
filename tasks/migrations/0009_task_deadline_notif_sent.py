98802aac5b641bfb60cb66597f03f0fd48cded74
Amy-Jassal <amy-jassal@hotmail.co.uk>
2023-12-08 14:24:21 +0000

    Working version once more

--- tasks/migrations/0009_task_deadline_notif_sent.py
+++ tasks/migrations/0009_task_deadline_notif_sent.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tasks', '0010_remove_task_deadline_keydates'),
+        ('tasks', '0008_remove_notification_task_name_user_notifications_and_more'),
     ]
 
     operations = [
